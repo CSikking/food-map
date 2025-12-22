10724aaa306607bd835bfe3723abdb0d4bf9ee1b
CSikking <148571051+CSikking@users.noreply.github.com>
2025-12-22 16:16:21 -0500

    Add files via upload

---
+++ restaurants-config.js
@@ -0,0 +1,22 @@
+const restaurants = [
+
+{name: "Giumarello's Restaurant & G Bar", lat: 39.905567, lng: -75.042747, reel: "https://www.instagram.com/reel/DSicND5AGqn/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+{name: "Bebo's Cafe", lat: 18.431544, lng: -66.002081, reel: ""},
+{name: "The Mezzanine", lat: 18.466616, lng: -66.116806, reel: ""},
+{name: "Cap'n Cat Clam Bar", lat: 39.633355, lng: -75.085565, reel: "https://www.instagram.com/reel/DSQan3cElb5/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+{name: "Fat Rabbit", lat: 30.262439, lng: -97.742899, reel: "https://www.instagram.com/reel/DSJA3ynEtYY/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+{name: "Razza", lat: 40.717771, lng: -74.044297, reel: "https://www.instagram.com/reel/DSGcDrqACQb/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+{name: "Joe's Bakery & Coffee Shop", lat: 30.261072, lng: -97.716685, reel: "https://www.instagram.com/reel/DRsXikbgHcr/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+{name: "Civil Goat", lat: 30.261418, lng: -97.72119, reel: "https://www.instagram.com/p/DRS8H08EuNU/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+{name: "Terry Black's BBQ", lat: 30.259717, lng: -97.754785, reel: "https://www.instagram.com/reel/DRPsaIygFWY/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+{name: "Ramen Tatsu-Ya", lat: 30.263176, lng: -97.726273, reel: "https://www.instagram.com/reel/DRiY-QwAOVg/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+{name: "Hillside Farmacy", lat: 30.268198, lng: -97.726921, reel: "https://www.instagram.com/reel/DRaV_aKALRW/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+{name: "Paperboy East", lat: 30.268482, lng: -97.727623, reel: "https://www.instagram.com/reel/DRIUV-IgBXn/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+{name: "Suerte", lat: 30.262278, lng: -97.723259, reel: "https://www.instagram.com/reel/DQsUQxTD0xe/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+{name: "Borromini", lat: 39.950452, lng: -75.170981, reel: "https://www.instagram.com/reel/DQ-VxuyALA7/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+{name: "Mattie's", lat: 30.24566, lng: -97.762222, reel: "https://www.instagram.com/reel/DRzIEW4APB6/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+{name: "Rockwell and Rose", lat: 39.948198, lng: -75.151837, reel: "https://www.instagram.com/reel/DQ2SyzNDw3d/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+{name: "The Goat", lat: 39.951177, lng: -75.172386, reel: "https://www.instagram.com/p/DQAV-mzEu-Q/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+{name: "Green Egg's Cafe", lat: 39.948083, lng: -75.162491, reel: "https://www.instagram.com/reel/DPKCnD-gKB1/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
+{name: "Darling Jack's Tavern", lat: 39.950139, lng: -75.162148, reel: "https://www.instagram.com/reel/DPSJCKLAC8u/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="}
+  ];
